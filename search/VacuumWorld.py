94a64a3ba583d0e34a5d58ac2835e662771353a2
Pedro GMR Basso <pedrogbasso@gmail.com>
2019-03-14 15:58:06 -0300

    Começo da resolução
    Não sei se está correto e possui alguns comentarios de dúvidas pessoais

--- search/VacuumWorld.py
+++ search/VacuumWorld.py
@@ -1,37 +1,62 @@
 from SearchAlgorithms import BuscaLargura
 from SearchAlgorithms import BuscaProfundidade
+from SearchAlforithms import BuscaProfundidadeIterativa
 from Graph import State
 
-# TODO
+# 
 # Implements a simplified version of vacuum world problem
+#
+# Initial state:
+#	VacuumPos: Left
+#	LeftRoom: Dirty
+#	RightRoom: Dirty
+# Operations: 
+#	Move Vacuum Left
+#	Move Vacuum Right
+#	Clean
+# Final state: 
+#	Both Rooms are clean
 #
 
 class VacuumWorld(State):
 
-    def __init__(self):
-        pass
+    def __init__(self, vacuumPosition, isLeftRoomClean, isRightRoomClean, op):
+        self.vacuumPosition = vacuumPostion
+		self.isLeftRoomClean = isLeftRoomClean
+		self.isRightRoomClean = isRightRoomClean
+		self.operator = op
     
     def sucessors(self):
-        pass
+        successors = []
+		
+	    #É necessario verificar a VacuumPos antes de mover a Vacuum?
+		
+		sucessors.append(VacuumWorld('right', self.isLeftRoomClean, self.isRightRoomClean, 'Move Right'))
+		sucessors.append(VacuumWorld('left', self.isLeftRoomClean, self.isRightRoomClean, 'Move Left'))
+		
+		#TODO: Implementar ações de limpeza. Importante: Verificar qual a VacuumPos e se a posição está suja
+		#Como designar sucessors condicionais?
+		
+		return sucessors
     
     def is_goal(self):
-        pass
+        return (isLeftRoomClean && isRightRoomClean)
     
     def description(self):
-        return "bla"
+        return "Problema do aspirador de pó, contendo duas salas"
     
     def cost(self):
         return 1
 
     def print(self):
-        pass
+        return str(self.operator)
 
 def main():
     
     #
     # Executando busca em largura
     #
-    state = None #TODO
+    state = VacuumWorld('left', false, false, '')
     algorithm = BuscaLargura()
     result = algorithm.search(state)
     if result != None:
@@ -43,7 +68,7 @@ def main():
     #
     # Executando busca em profundidade
     #
-    state = None #TODO
+    state = VacuumWorld('left', false, false, '')
     algorithm = BuscaProfundidade()
     result = algorithm.search(state, 10)
     if result != None:
@@ -54,4 +79,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
