c3ac5fbf1491fda57e5c9e78512f2917f7ac79bc
Fabricio Barth <fbarth@br.ibm.com>
2019-03-18 18:29:47 -0300

    Initial version of VacuumWorld

--- search/VacuumWorld.py
+++ search/VacuumWorld.py
@@ -2,36 +2,51 @@ from SearchAlgorithms import BuscaLargura
 from SearchAlgorithms import BuscaProfundidade
 from Graph import State
 
-# TODO
+# 
 # Implements a simplified version of vacuum world problem
 #
 
 class VacuumWorld(State):
 
-    def __init__(self):
-        pass
+    def __init__(self, left, right, vacuum, op):
+        self.left = left
+        self.right = right
+        self.vacuum = vacuum
+        self.operator = op
     
     def sucessors(self):
-        pass
+        sucessors = []
+        # ir para esquerda
+        sucessors.append(VacuumWorld(self.left, self.right, 'left', 'ir p/ esquerda'))
+        #ir para direita
+        sucessors.append(VacuumWorld(self.left, self.right, 'right', 'ir p/ esquerda'))
+        #limpar
+        if self.vacuum == 'left':
+            sucessors.append(VacuumWorld('limpo', self.right, self.vacuum, 'limpar'))
+        else:
+            sucessors.append(VacuumWorld(self.left, 'limpo', self.vacuum, 'limpar'))
+        return sucessors
     
     def is_goal(self):
-        pass
+        if (self.left == 'limpo') and (self.right == 'limpo'):
+            return True
+        return False
     
     def description(self):
-        return "bla"
+        return "Implementa o problema do aspirador de pó"
     
     def cost(self):
         return 1
 
     def print(self):
-        pass
+        return str(self.operator)
 
 def main():
     
     #
     # Executando busca em largura
     #
-    state = None #TODO
+    state = VacuumWorld('sujo', 'sujo', 'left', '')
     algorithm = BuscaLargura()
     result = algorithm.search(state)
     if result != None:
@@ -43,7 +58,7 @@ def main():
     #
     # Executando busca em profundidade
     #
-    state = None #TODO
+    state = VacuumWorld('sujo', 'sujo', 'left', '')
     algorithm = BuscaProfundidade()
     result = algorithm.search(state, 10)
     if result != None:
