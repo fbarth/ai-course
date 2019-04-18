cd3557d05d0e44e3c4c2c7c2046579313e8bba4a
Fabricio Barth <fbarth@br.ibm.com>
2019-04-18 12:16:33 -0300

    Returning with the original file of TaxiDriver

--- search/TaxiDriver.py
+++ search/TaxiDriver.py
@@ -3,49 +3,34 @@ from Graph import State
 
 class TaxiDriver(State):
 
-    def __init__(self, taxiPosition, matrix, positionDestiny, passenger, op):
-        self.taxiPosition = taxiPosition
-        self.matrix = matrix
-        self.positionDestiny = positionDestiny
+    def __init__(self, op):
         self.operator = op
-        self.passenger = passenger
+        #TODO
     
     def sucessors(self):
         sucessors = []
-        #para cima
-        if (self.taxiPosition[0]-1 >= 0):
-            sucessors.append(TaxiDriver([self.taxiPosition[0]-1,self.taxiPosition[1]], self.matrix, self.positionDestiny, 'cima'))
-        #para baixo
-        if (self.taxiPosition[0]+1 < len(self.matrix) and self.taxiPosition[0]+1 > 0):
-            sucessors.append(TaxiDriver([self.taxiPosition[0]+1, self.taxiPosition[1]], self.matrix, self.positionDestiny, 'baixo'))
-        #para esquerda
-        if (self.taxiPosition[1]-1 >= 0):
-            sucessors.append(TaxiDriver([self.taxiPosition[0], self.taxiPosition[1]-1], self.matrix, self.positionDestiny, 'esquerda'))
-        #para direita
-        if (self.taxiPosition[1]+1 < len(self.matrix[0]) and self.taxiPosition[1]+1 > 0 ):
-            sucessors.append(TaxiDriver([self.taxiPosition[0],self.taxiPosition[1]+1], self.matrix, self.positionDestiny, 'direita'))
+        #TODO
         return sucessors
     
     def is_goal(self):
-        return(self.taxiPosition == self.positionDestiny)
+        pass
     
     def description(self):
         return "Describe the problem"
     
     def cost(self):
-        return 1
+        pass
 
     def print(self):
         return str(self.operator)
 
     def h(self):
-        return 1
+        pass
 
 
 def main():
     print('Busca A *')
-    matrix = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-    state = TaxiDriver([0,0], matrix, [3,2],'')
+    state = TaxiDriver('')
     algorithm = AEstrela()
     result = algorithm.search(state)
     if result != None:
