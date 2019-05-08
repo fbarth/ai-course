4ed2b16ccc78c195800fb9ef942f1b5c68b112b1
ejmeri <elmerisilva@hotmail.com>
2019-05-08 19:41:19 -0300

    printenv

--- search/TaxiDriverEJ-FELIPE.py
+++ search/TaxiDriverEJ-FELIPE.py
@@ -12,12 +12,14 @@ class TaxiDriver(State):
         self.took = took
         self.blocks = blocks
         self.drop = drop
-    
+
+    def printEnv(self):
+        return str(self.took)+"   "+str(self.taxi) #self.customer
 
     def sucessors(self):
         sucessors = []
 
-        if (self.taxi_can_go(self.taxi[0], self.taxi[1], self.blocks)): 
+        if (self.took == False and self.taxi_can_go(self.taxi[0], self.taxi[1], self.blocks)): 
             if (self.taxi[0] - 1 >= 0):
                 sucessors.append(TaxiDriver([self.taxi[0]-1, self.taxi[1]], self.matrix, self.where, self.customer,False, self.blocks, False, 'up'))
             
@@ -46,8 +48,8 @@ class TaxiDriver(State):
             if (self.taxi[1] + 1 < len(self.matrix[0]) and self.taxi[1] + 1 > 0):
                 sucessors.append(TaxiDriver([self.taxi[0], self.taxi[1] + 1], self.matrix, self.where, [self.customer[0], self.customer[1] + 1], True, self.blocks, False,'right'))
         
-        if(self.took == True and self.taxi == self.where and self.customer == self.where):
-            sucessors.append(TaxiDriver(self.taxi, self.matrix, self.where, self.customer, False, self.blocks, True, 'drop customer'))
+        if(self.took == True and self.taxi == self.where):
+            sucessors.append(TaxiDriver(self.taxi, self.matrix, self.where, self.customer, True, self.blocks, True, 'drop customer'))
         
         return sucessors
     
@@ -65,9 +67,9 @@ class TaxiDriver(State):
 
     def h(self):
         if (self.took):
-            return abs(self.customer[0] - self.where[0]) + abs(self.customer[1] - self.where[1])
+            return abs(self.taxi[0] - self.customer[0]) + abs(self.taxi[1] - self.customer[1]) 
         else:
-            return abs(self.taxi[0] - self.where[0]) + abs(self.taxi[1] - self.where[1])
+            return abs(self.taxi[0] - self.where[0]) + abs(self.taxi[1] - self.where[1]) + 100
     
     def taxi_can_go(self, x, y, obs):
         for i in obs:
@@ -78,8 +80,10 @@ class TaxiDriver(State):
 
 def main():
     print('Busca A*')
-    matrix = [[0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0],[0,0,0], [0,0,0], [0,0,0], [0,0,0]]
-    state = TaxiDriver([0,0], matrix, [9,2], [38,17], False, [[0,1], [1,1], [2,1]], False, '')
+    #matrix = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+    matrix = [[0 for col in range(6)] for row in range(6)]
+    #state = TaxiDriver([0,0], matrix, [1,2], [1,1], False, [[0,1], [1,1], [2,1]], False, '')
+    state = TaxiDriver([5,0], matrix, [5,5], [2,3], False, [[1,2],[2,2],[3,2],[4,2],[5,2],[1,3]], False, '')
     algorithm = AEstrela()
     result = algorithm.search(state)
     if result != None:
