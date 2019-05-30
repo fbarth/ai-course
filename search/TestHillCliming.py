90566ab1f3339b17e7c9e79d400e05b0e05f7090
Fabricio Barth <fbarth@br.ibm.com>
2019-05-29 21:13:05 -0300

    Adding a specific file to test the hill climing algorithm

--- search/TestHillCliming.py
+++ search/TestHillCliming.py
@@ -1,6 +1,5 @@
 #
-# TODO implement a solution for N-queens problem, where N could 
-# be any number between 4 and 10.
+# This class is a test for the hill climing algorithm
 #
 
 from SearchAlgorithms import SubidaMontanha
