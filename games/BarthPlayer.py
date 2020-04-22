d32be5e49cdba29eb3ff925a44d9773ffdd7e998
Fabricio Barth <fabricio.barth@gmail.com>
2020-04-22 18:27:05 -0300

    exemplo para o jogo connect-4

---
+++ games/BarthPlayer.py
@@ -0,0 +1,32 @@
+from Player import Player
+
+class PlayerSpecification(Player):
+
+    def move(self, player_code, board):
+        sucessores = self.sucessores(player_code, board)
+        # for each sucessor in sucessores calcular o eval
+        return None
+
+    def sucessores(self, player_code, board):
+        suc = []
+        for i in range(0,7):
+            mov = self.movement(player_code, board, i)
+            if(mov != None):
+                suc.append()
+        return suc
+
+    def eval(self, board):
+        # se o meu oponente tem quatro em uma linha entao -100000
+        # senao 100000*(#4-in-row) + 100*(#3-in-row) + (#2-in-row)
+        return None
+    
+    def movement(self, player, board, column):
+        result_board = board
+        for i in range(5,-2,-1):
+            if (board[i,column] == 0):
+                break
+        if(i<0):
+            return None
+        result_board[i, column] = player
+        return result_board
+
