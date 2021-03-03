1d7006fc3072275f35893f81585cb133333bf2aa
fbarth <fbarth@br.ibm.com>
2021-03-03 16:01:09 -0300

    terminando enunciado do primeiro exercicio

--- README.md
+++ README.md
@@ -9,5 +9,22 @@ I am using this project to support the exercises in an Artificial Intelligence c
 5.	Game Playing, and;
 6.	Reinforcement Learning (I present in this course the concept of Learning from Observations, Supervised Learning and Unsupervised Learning. However, I have a specific course to discuss those topics. You can access this material [here](https://github.com/fbarth/ml-espm)).
 
-If you prefer, you can see the description of the course in this [video](https://www.youtube.com/watch?v=LsD24XDhIHg). 
+If you prefer, you can see the description of this course in this [video](https://www.youtube.com/watch?v=LsD24XDhIHg). 
+
+## How this project is structured? 
+
+We have four (4) folders:
+
+* *docs*: this folder has several documents describing concepts, algorithms and projects. All content is in Portuguese. 
+
+* *search*: this folder has implementations related to solving problem by searching, and informed search methods. I suggest to start from this folder in order to understand all the implementations (search, games, and reinLearn).
+
+* *games*: this folder keeps source code related to game playing.
+
+* *reinLearn*: this folder has source code related to reinforcement learning. Some examples are using the project [Gym](https://gym.openai.com/).
+
+## How to setup the environment? 
+
+In order to avoid any problem, I recommend ... (TODO)
+
 
--- search/PlusOneTwo.py
+++ search/PlusOneTwo.py
@@ -16,7 +16,7 @@ from Graph import State
 
 class PlusOneTwo(State):
 
-    Goal = 100
+    Goal = 10
 
     def __init__(self, number, op):
         self.number = number
@@ -56,74 +56,74 @@ def main():
     #
     # Executando busca em largura
     #
-    # print('Busca em largura')
-    # state = PlusOneTwo(1, '')
-    # algorithm = BuscaLargura()
-    # inicio = datetime.now()
-    # result = algorithm.search(state)
-    # fim = datetime.now()
-    # print(fim - inicio)
-    # if result != None:
-    #     print('Achou!')
-    #     print(result.show_path())
-    # else:
-    #     print('Nao achou solucao')
-    #
+    print('Busca em largura')
+    state = PlusOneTwo(1, '')
+    algorithm = BuscaLargura()
+    inicio = datetime.now()
+    result = algorithm.search(state)
+    fim = datetime.now()
+    print(fim - inicio)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
+    
     #
     # Executando busca em profundidade
     #
-    # print('Busca em profundidade')
-    # state = PlusOneTwo(1, '')
-    # algorithm = BuscaProfundidade()
-    # result = algorithm.search(state, 50)
-    # if result != None:
-    #    print('Achou!')
-    #    print(result.show_path())
-    # else:
-    #    print('Nao achou solucao')
+    print('Busca em profundidade')
+    state = PlusOneTwo(1, '')
+    algorithm = BuscaProfundidade()
+    result = algorithm.search(state, 50)
+    if result != None:
+       print('Achou!')
+       print(result.show_path())
+    else:
+       print('Nao achou solucao')
         
     #
     # Executando busca em profundidade iterativa
     #
-    # print('Busca em profundidade iterativa')
-    # state = PlusOneTwo(1, '')
-    # algorithm = BuscaProfundidadeIterativa()
-    # result = algorithm.search(state)
-    # if result != None:
-    #    print('Achou!')
-    #    print(result.show_path())
-    # else:
-    #    print('Nao achou solucao')
+    print('Busca em profundidade iterativa')
+    state = PlusOneTwo(1, '')
+    algorithm = BuscaProfundidadeIterativa()
+    result = algorithm.search(state)
+    if result != None:
+       print('Achou!')
+       print(result.show_path())
+    else:
+       print('Nao achou solucao')
 
-    # #
-    # # Executando busca de custo uniforme
-    # #
-    # print('Busca de custo uniforme')
-    # state = PlusOneTwo(1, '')
-    # algorithm = BuscaCustoUniforme()
-    # result = algorithm.search(state)
-    # if result != None:
-    #     print('Achou!')
-    #     print(result.show_path())
-    # else:
-    #     print('Nao achou solucao')
+    #
+    # Executando busca de custo uniforme
+    #
+    print('Busca de custo uniforme')
+    state = PlusOneTwo(1, '')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
 
-    # #
-    # # Executando busca gananciosa
-    # #
-    #print('Busca Gananciosa')
-    #state = PlusOneTwo(1, '')
-    #algorithm = BuscaGananciosa()
-    #result = algorithm.search(state)
-    #if result != None:
-    #    print('Achou!')
-    #    print(result.show_path())
-    #else:
-    #    print('Nao achou solucao')
+"""     #
+    # Executando busca gananciosa
+    #
+    print('Busca Gananciosa')
+    state = PlusOneTwo(1, '')
+    algorithm = BuscaGananciosa()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
 
-    ##
-    ## Executando busca A*
-    ##
+    #
+    # Executando busca A*
+    #
     print('Busca A*')
     state = PlusOneTwo(1, '')
     algorithm = AEstrela()
@@ -132,7 +132,7 @@ def main():
         print('Achou!')
         print(result.show_path())
     else:
-        print('Nao achou solucao')
+        print('Nao achou solucao') """
         
 
 if __name__ == '__main__':
